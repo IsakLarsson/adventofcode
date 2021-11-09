27c24be4388e3a9023399d9cdd91257c28ca2d23
Isak Larsson <55209313+IsakLarsson@users.noreply.github.com>
2021-11-09 19:41:23 +0100

    day2 part 1

---
+++ 2020/Day2.py
@@ -0,0 +1,31 @@
+with open('inputs/Day2Input.txt') as f:
+	lines = f.readlines()
+	
+def part1():
+	validCounter = 0
+	for line in lines:
+		policy, password = line.split(':')
+		lowerRange, upperAndLetter = policy.split('-')
+		upperRange, letter = upperAndLetter.split()
+		if password.count(letter) >= int(lowerRange) and password.count(letter) <= int(upperRange):
+			validCounter+=1
+
+	print(validCounter)
+
+def part2():
+	validCounter = 0
+	for line in lines:
+		policy, password = line.split(':')
+		firstPos, upperAndLetter = policy.split('-')
+		secondPos, letter = upperAndLetter.split()
+		print(password.find(letter), letter, password)
+		# if (password.find(letter)+1 == int(firstPos) and password.find(letter)+1 != int(secondPos)) or (password.find(letter)+1 == int(secondPos) and password.find(letter)+1 != int(firstPos)):
+		# 	print(firstPos,secondPos,letter,password)
+		# 	validCounter+=1
+
+	print(validCounter)
+
+part1()
+part2()
+
+f.close()
