389ef177b9f833dea2d052f4f3be45522c490ca8
Isak Larsson <55209313+IsakLarsson@users.noreply.github.com>
2021-11-10 17:49:02 +0100

    day 4 part 1

--- 2020/Day4.py
+++ 2020/Day4.py
@@ -3,15 +3,25 @@ with open('2020/inputs/day4input.txt') as f:
 	content = f.readlines()
 
 #if 2 \n's are in a row then there is a new line
-print(content)
+# print(content)
 
-passports = [[]]
+passports = []
 
-passportIndex = 0
+requiredFields = {'eyr', 'hgt','hcl','pid', 'byr', 'iyr', 'ecl'} 
+
+validPasstports = 0
+foundFields = set()
 for line in content:
-	passports[passportIndex].append(line) 
+
 	if line == '\n':
 		#new passport
-		passportIndex += 1
+		if requiredFields.issubset(foundFields):
+			validPasstports += 1
+		foundFields.clear()
 
-print(passports)
+	else:
+		splitLine = line.split(' ')
+		[foundFields.add(x.split(':')[0]) for x in splitLine]
+		
+
+print(validPasstports)
