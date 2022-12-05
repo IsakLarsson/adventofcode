cd09307a1af7a95941183462e86e864005b7867a
Isak Larsson <isaklarsson98@hotmail.com>
2022-12-05 19:24:05 +0100

    Finally got day3 part 2 right, its ugly but idc

--- 2022/day3/solution2.js
+++ 2022/day3/solution2.js
@@ -1,19 +1,41 @@
 const fs = require('fs')
+const { CLIENT_RENEG_LIMIT } = require('tls')
 const rucksacks = fs
     .readFileSync('./input.txt', { encoding: 'utf8' })
     .split('\n')
 
+const calcPriority = (character) => {
+    if (character.charCodeAt(0) - 97 < 0) {
+        //uppercase
+        return character.charCodeAt(0) - 64 + 26
+    } else {
+        return character.charCodeAt(0) - 96
+    }
+}
 let sum = 0
 let line = 0
-rucksacks.forEach((rucksack) => {
-    let group = []
-    group.push(rucksack)
-    console.log(group)
-    if (line % 3 === 0) {
-        //start new group
-        group = []
+let group = []
+rucksacks.forEach((groupString) => {
+    if (groupString === '') {
         return
     }
+    group.push(groupString)
+    if (line % 3 === 2) {
+        const first = [...group[0]]
+        const second = group[1]
+        const third = group[2]
+        console.log(group)
+        for (let index = 0; index <= first.length; index++) {
+            if (second.includes(first[index]) && third.includes(first[index])) {
+                console.log(first[index])
+                sum += calcPriority(first[index])
+                group = []
+                line++
+                return
+            }
+        }
+    }
+
     line++
 })
 console.log(sum)
