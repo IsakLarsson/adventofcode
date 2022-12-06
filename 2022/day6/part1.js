ffdab9792c5f493d393dfc9779d4468f4195822c
Isak Larsson <isaklarsson98@hotmail.com>
2022-12-06 08:10:30 +0100

    Super compact but highly unreadable lol

--- 2022/day6/part1.js
+++ 2022/day6/part1.js
@@ -2,16 +2,10 @@ const fs = require('fs')
 const signal = fs.readFileSync('./input.txt', 'utf8')
 
 let window = []
-const checkDifferent = (window) => {
-    return new Set(window).size === 4
-}
 for (let index = 0; index < signal.length; index++) {
     window.push(signal[index])
-    if (window.length > 4) {
-        window.splice(0, 1)
-    }
-    if (checkDifferent(window) === true) {
-        console.log(index + 1)
-        return
-    }
+    if (window.length > 4)
+        window.splice(0, 1)(Set(window).size === 4(window)) === true
+            ? console.log(index + 1)
+            : ''
 }
