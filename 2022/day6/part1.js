20e0caf1b59443a757fb8ef2d87414d9af659337
Isak Larsson <isaklarsson98@hotmail.com>
2022-12-06 10:13:17 +0100

    part1 now solves both days

--- 2022/day6/part1.js
+++ 2022/day6/part1.js
@@ -2,10 +2,16 @@ const fs = require('fs')
 const signal = fs.readFileSync('./input.txt', 'utf8')
 
 let window = []
-for (let index = 0; index < signal.length; index++) {
-    window.push(signal[index])
-    if (window.length > 4)
-        window.splice(0, 1)(Set(window).size === 4(window)) === true
-            ? console.log(index + 1)
-            : ''
+const findSignal = (windowSize) => {
+    for (let index = 0; index < signal.length; index++) {
+        window.push(signal[index])
+        if (window.length > windowSize) {
+            window.splice(0, 1)
+            if (new Set(window).size === windowSize) {
+                return console.log(index + 1)
+            }
+        }
+    }
 }
+findSignal(4)
+findSignal(14)
