5939b952c59ee29d4d328be4e9c4b4e4e3bdecf0
Isak Larsson <isak.larsson@savr.com>
2024-12-03 11:41:44 +0100

    clean up

--- 2024/day3/solution.py
+++ 2024/day3/solution.py
@@ -1,16 +1,11 @@
 import re 
 
-input = open("input.txt", "r").read().split("\n")
+input = open("input.txt", "r").read()
 
-mults =[]
 sum =0
+for match in (re.findall("mul\\(\\d{1,3},\\d{1,3}\\)", input)):
+    tup = tuple(re.findall("\\d{1,3}", match))
+    sum += int(tup[0]) * int(tup[1])
 
-for line in input:
-    for match in (re.findall("mul\\(\\d{1,3},\\d{1,3}\\)", line)):
-        mults.append(tuple(re.findall("\\d{1,3}", match)))
-
-for pair in mults:
-   sum += int(pair[0]) * int(pair[1])
-        
 
 print(sum)
